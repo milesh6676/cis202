0a2109ebd9e853638686b856ecf61ab896e1b334
milesh6676 <129228807+milesh6676@users.noreply.github.com>
2023-06-05 01:32:50 +0000

    6/4/2023

---
+++ projects/tree-project/tree.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include "tree.h"
+
+Tree::Tree()
+{
+   root = nullptr;
+}
+
+Tree::Tree(std::string root_data)
+{
+   root = new Node;
+   root->data = root_data;
+}
+
+void Tree::add_subtree(const Tree& subtree)
+{
+   root->children.push_back(subtree.root);
+}
+
+int Tree::size() const
+{
+   if (root == nullptr) { return 0; }
+   else { return root->size(); }
+}
+
+void Tree::print() const {
+   if( root != nullptr ) {
+      root->print();
+   }
+   
+}
+
+int Node::size() const
+{
+   int sum = 0;
+   for (Node* child : children)
+   {
+      sum = sum + child->size();
+   }
+   return 1 + sum;
+}
+
+void Node::print() const {
+   std::cout << data << std::endl;
+   for( Node* np : children ) {
+      np->print();
+   }
+}
